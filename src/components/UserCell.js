cae738e863f6856ea16e816a71d78abd84777477
stephane job <darksh3ll.js@gmail.com>
2019-09-03 00:50:30 +0200

    props user api

--- src/components/UserCell.js
+++ src/components/UserCell.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import "../assets/styles/usercell.css"
 
-
-const UserCell = () => {
-  return (
-    <div />
-  );
+const UserCell = ( item ) => {
+    const { avatar_url, login,html_url } = item
+    return (
+        <div className="container">
+            <img className="avatarImg" src={avatar_url} alt="avatar" />
+            <p className="login">{login}</p>
+            <a href={html_url} rel="noopener" target="_blank">github</a>
+        </div>
+    );
 }
-
 
 export default UserCell
