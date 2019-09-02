815155132918c4766c0faeb537b5f6627048e715
stephane job <darksh3ll.js@gmail.com>
2019-09-02 22:01:31 +0200

    initialize  state

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 
 export default class screens extends Component {
+    state ={
+        value : "",
+        users:[],
+    }
     render() {
         return (
             <h1>Home</h1>
