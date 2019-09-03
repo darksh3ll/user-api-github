57e6746bf20ffee523882e8a9977ca47f79fb3c5
stephane <darksh3ll.js@gmail.com>
2019-09-03 14:57:13 +0200

    refactoring

--- src/components/UserCell.js
+++ src/components/UserCell.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import "../assets/styles/usercell.css"
+import "../assets/styles/userCell.css"
 
-const UserCell = ( item ) => {
-    const { avatar_url, login,html_url } = item
-    return (
-        <div className="container">
-            <img className="avatarImg" src={avatar_url} alt="avatar" />
-            <p className="login">{login}</p>
-            <a href={html_url} rel="noopener" target="_blank">github</a>
-        </div>
-    );
-}
+const UserCell = (item) => {
+  const {avatar_url, login, html_url} = item
+  return (
+    <div className="container">
+      <img className="avatarImg" src={avatar_url} alt="avatar"/>
+      <p className="login">{login}</p>
+      <a href={html_url}>view profile</a>
+    </div>
+  );
+};
 
 export default UserCell
